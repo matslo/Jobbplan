2ff47460697e5713426d96b28a9b275b1ab00fe4
Atle Aspelien <atleaspelien@hotmail.com>
2016-01-26 14:24:39 +0100

    Projektdeltagelse klasse

--- Jobbplan/Models/Prosjektdeltakelse.cs
+++ Jobbplan/Models/Prosjektdeltakelse.cs
@@ -2,11 +2,17 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
-
+using System.ComponentModel.DataAnnotations;
 namespace Jobbplan.Models
 {
     public class Prosjektdeltakelse
     {
-       
+        [Key]
+       public int ProsjektDeltagerId { get; set; }
+        public int BrukerId { get; set; }
+        public int ProsjektId { get; set; }
+        public DateTime Start { get; set; }
+        public DateTime Slutt { get; set; }
+
     }
 }
