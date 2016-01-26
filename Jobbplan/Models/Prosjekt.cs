7e1c369f1ddbe405d7907e9af5cb248bac47108c
Atle Aspelien <atleaspelien@hotmail.com>
2016-01-26 10:58:54 +0100

    Prosjekt klasse

--- Jobbplan/Models/Prosjekt.cs
+++ Jobbplan/Models/Prosjekt.cs
@@ -2,10 +2,15 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.ComponentModel.DataAnnotations;
 
 namespace Jobbplan.Models
 {
     public class Prosjekt
     {
+        [Key]
+        public int ProsjektId { get; set; }
+        public int BrukerId { get; set; }
+        public string Arbeidsplass { get; set; }
     }
 }
