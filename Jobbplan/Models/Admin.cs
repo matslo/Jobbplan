4034759ae9fd370b206c70cd18ccc189e40fbefe
Atle Aspelien <atleaspelien@hotmail.com>
2016-01-26 14:28:04 +0100

    Admin klasse

--- Jobbplan/Models/Admin.cs
+++ Jobbplan/Models/Admin.cs
@@ -2,10 +2,16 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
-
+using System.ComponentModel.DataAnnotations;
 namespace Jobbplan.Models
 {
     public class Admin
     {
+        [Key]
+        public int AbminId { get; set; }
+        public int ProsjektDeltagerId { get; set; }
+        public int ProsjektId { get; set; }
+        public string Rettigheter { get; set; }
+        public string Tittel { get; set; }
     }
 }
