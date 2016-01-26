874570179c87a32da616a2bdf48b89daee09afb3
Atle Aspelien <atleaspelien@hotmail.com>
2016-01-26 11:13:48 +0100

    Timeliste klasse

--- Jobbplan/Models/Timeliste.cs
+++ Jobbplan/Models/Timeliste.cs
@@ -2,10 +2,21 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.ComponentModel.DataAnnotations;
 
 namespace Jobbplan.Models
 {
     public class Timeliste
     {
+        [Key]
+        public int BrukerId { get; set; }
+        public int Timer { get; set; }
+        public string Fornavn { get; set; }
+        public string Etternavn { get; set; }
+        public DateTime Start { get; set; }
+        public DateTime Slutt { get; set; }
+        public decimal Sum { get; set; }
+
     }
+
 }
