4f462e7126810baca642587ce78fad1297d5cfaf
mats løkken <mats_loekken@hotmail.com>
2016-04-06 13:55:32 +0200

    Endret design

---
+++ Jobbplan/Controllers/SjefController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Jobbplan.Models;
+
+namespace Jobbplan.Controllers
+{
+    public class SjefController : ApiController
+    {
+        DbTransaksjonerBruker db = new DbTransaksjonerBruker();
+        public HttpResponseMessage Post(Sjef innBruker)
+        {
+            string userName = User.Identity.Name;
+          
+                bool ok = db.GiBrukerAdminTilgang(innBruker);
+                if (ok)
+                {
+                    return new HttpResponseMessage()
+                    {
+                        StatusCode = HttpStatusCode.Created,
+                    };
+                }
+            
+            return new HttpResponseMessage()
+            {
+                StatusCode = HttpStatusCode.NotFound,
+                Content = new StringContent("Feil")
+            };
+        }
+        public HttpResponseMessage Put(Sjef innBruker)
+        {
+            string userName = User.Identity.Name;
+
+            bool ok = db.FjernAdminTilgang(innBruker);
+            if (ok)
+            {
+                return new HttpResponseMessage()
+                {
+                    StatusCode = HttpStatusCode.Created,
+                };
+            }
+
+            return new HttpResponseMessage()
+            {
+                StatusCode = HttpStatusCode.NotFound,
+                Content = new StringContent("Feil")
+            };
+        }
+    }
+}
